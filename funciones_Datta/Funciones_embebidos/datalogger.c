442b40534e5af5a4ddde6e99c3f0279f7a4caab1
juan felipe <juanfelipeperdomorojas@gmail.com>
2024-04-18 10:46:02 -0300

    funciones

---
+++ funciones_Datta/Funciones_embebidos/datalogger.c
@@ -0,0 +1,25 @@
+#include "datalogger.h"
+#include "main.h"
+
+
+void GetTemperatura(char *timeStr ){
+int parte_entera;
+int parte_decimal;
+int adc_value;
+int adc_value1;
+char Cadena[100];
+
+	HAL_ADC_Start(&hadc1);
+		if (HAL_ADC_PollForConversion(&hadc1, 100) == HAL_OK) {
+		      adc_value = HAL_ADC_GetValue(&hadc1);
+		}
+		HAL_ADC_Stop(&hadc1);
+		HAL_Delay(500);
+		adc_value1=(((adc_value*100000)/4096)*3.3*100)-5000000;
+		parte_entera=adc_value1/100000;
+		parte_decimal=abs(adc_value1%(parte_entera*100000));
+		parte_decimal=parte_decimal/1000;
+		sprintf(Cadena,"%s %d,%0.2d °C\n\r", timeStr, parte_entera, parte_decimal);
+		HAL_UART_Transmit(&huart3, (uint8_t *)Cadena, strlen(Cadena), HAL_MAX_DELAY);
+}
+
---
+++ funciones_Datta/Funciones_embebidos/datalogger.h
@@ -0,0 +1,4 @@
+#ifndef INC_DATALOGGER_H_
+#define INC_DATALOGGER_H_
+ void GetTemperatura(char *Cadena,char *timeStr );
+ #endif
